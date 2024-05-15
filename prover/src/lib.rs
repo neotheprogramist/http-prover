3719e4d0476bb9f83ed70e78e972ddb86ab78766
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2024-05-15 13:48:17 +0200

    cli

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -7,12 +7,20 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
     /// Host address to bind the server
-    #[clap(long, default_value = "0.0.0.0")]
+    #[arg(long, default_value = "0.0.0.0")]
     host: String,
 
     /// Port to listen on
-    #[clap(long, default_value = "3000")]
+    #[arg(long, default_value = "3000")]
     port: u16,
+    #[arg(long, env)]
+    env_var_jwt_secret_key: String,
+    #[arg(long, env)]
+    message_expiration_time: u16,
+    #[arg(long, env)]
+    session_expiration_time: u16,
+    #[arg(long, env)]
+    private_key: String,
 }
 pub trait ProverInput {
     fn serialize(self) -> serde_json::Value;
