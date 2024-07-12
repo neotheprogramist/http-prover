760ff85ea1eb8428d7684f98d9f2cd69d408af21
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2024-07-12 18:10:20 +0200

    SSL certificate procedure

--- prover/src/server.rs
+++ prover/src/server.rs
@@ -5,16 +5,21 @@ use crate::{
     },
     prove, Args,
 };
+use acme_controller::CliInput;
 use axum::Router;
+use clap::Parser;
+use lib_acme::cert::cert_manager::{issue_certificate, read_cert, renew_certificate, save_cert};
 use prove::errors::ServerError;
 use std::{
     collections::HashMap,
+    path::Path,
     sync::{Arc, Mutex},
 };
 use std::{net::SocketAddr, time::Duration};
 use tokio::net::TcpListener;
 use tower_http::{limit::RequestBodyLimitLayer, timeout::TimeoutLayer, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use url::Url;
 use utils::shutdown::shutdown_signal;
 
 #[derive(Debug, Clone)]
@@ -74,10 +79,51 @@ pub async fn start(args: Args) -> Result<(), ServerError> {
     // Create a `TcpListener` using tokio.
     let listener = TcpListener::bind(address).await?;
 
+    tokio::spawn(async move {
+        let result = async {
+            let args = CliInput::new();
+            let domain_identifiers: Vec<&str> = args.domain_identifiers();
+            let contact_mails: Vec<String> = args.contact_mails.clone();
+            let challange_type = lib_acme::cert::types::ChallangeType::Dns01;
+            let api_token: &str = args.api_token.as_str();
+            let zone_id: &str = args.zone_id.as_str();
+            let path = Path::new(&args.cert_path);
+            let dir_url: &Url = &args.url;
+            let renewal_threshold = args.renewal_threshold;
+    
+            issue_certificate(
+                contact_mails.clone(),
+                domain_identifiers.clone(),
+                challange_type.clone(),
+                api_token,
+                zone_id,
+                dir_url,
+                path,
+            )
+            .await?;
+    
+            let cert = read_cert(path)?;
+    
+            renew_certificate(
+                contact_mails,
+                domain_identifiers,
+                challange_type,
+                api_token,
+                zone_id,
+                dir_url,
+                &cert,
+                path,
+                renewal_threshold,
+            )
+            .await?;
+    
+            Ok::<(), lib_acme::cert::errors::AcmeErrors>(())
+        }.await;
+    });
+
     // Run the server with graceful shutdown
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await?;
-
     Ok(())
 }
