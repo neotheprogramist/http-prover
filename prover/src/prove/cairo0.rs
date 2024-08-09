f00b245f60757b735b897fc1b05024b4ad87cbd7
Piotr Stec <piotr.stec2g@gmail.com>
2024-08-09 15:41:22 +0200

    Removing files

--- prover/src/prove/cairo0.rs
+++ prover/src/prove/cairo0.rs
@@ -14,9 +14,17 @@ pub async fn root(
     _claims: Claims,
     Json(program_input): Json<Cairo0ProverInput>,
 ) -> Result<String, ProveError> {
-    let program_input_path: PathBuf = Path::new("resources/CairoZero/input.json").to_path_buf();
-    let program_path: PathBuf = Path::new("resources/CairoZero/program.json").to_path_buf();
+    let program_input_path: PathBuf = Path::new("resources/cairoZero/input.json").to_path_buf();
+    let program_path: PathBuf = Path::new("resources/cairoZero/program.json").to_path_buf();
     let proof_path: PathBuf = Path::new("program_proof_cairo0.json").to_path_buf();
+    let trace_file = Path::new("resources/cairoZero/program_trace.trace").to_path_buf();
+    let memory_file = Path::new("resources/cairoZero/program_memory.memory").to_path_buf();
+    let public_input_file =
+        Path::new("resources/cairoZero/program_public_input.json").to_path_buf();
+    let private_input_file =
+        Path::new("resources/cairoZero/program_private_input.json").to_path_buf();
+    let params_file = Path::new("resources/cairoZero/cpu_air_params.json").to_path_buf();
+    let config_file = Path::new("config/cpu_air_prover_config.json").to_path_buf();
 
     let input = serde_json::to_string(&program_input.program_input)?;
     let program = serde_json::to_string(&program_input.program)?;
@@ -28,43 +36,60 @@ pub async fn root(
     //run cairo-run
     let mut command = Command::new("cairo-run");
     command
-        .arg("--trace_file=resources/CairoZero/program_trace.trace")
-        .arg("--memory_file=resources/CairoZero/program_memory.memory")
+        .arg("--trace_file")
+        .arg(&trace_file)
+        .arg("--memory_file")
+        .arg(&memory_file)
         .arg("--layout")
         .arg(layout)
         .arg("--proof_mode")
-        .arg("--air_public_input=resources/CairoZero/program_public_input.json")
-        .arg("--air_private_input=resources/CairoZero/program_private_input.json")
+        .arg("--air_public_input")
+        .arg(&public_input_file)
+        .arg("--air_private_input")
+        .arg(&private_input_file)
         .arg("--program_input")
-        .arg(program_input_path)
+        .arg(&program_input_path)
         .arg("--program")
-        .arg(program_path);
+        .arg(&program_path);
 
     let mut child = command.spawn()?;
     let _status = child.wait()?;
 
     generate_config::generate(
-        "resources/CairoZero/program_public_input.json",
-        "resources/CairoZero/cpu_air_params.json",
+        "resources/cairoZero/program_public_input.json",
+        "resources/cairoZero/cpu_air_params.json",
     );
 
     //run cpu_air_prover
     let mut command_proof = Command::new("cpu_air_prover");
     command_proof
-        .arg("--public_input_file=resources/CairoZero/program_public_input.json")
-        .arg("--private_input_file=resources/CairoZero/program_private_input.json")
-        .arg("--prover_config_file=examples/CairoZero/cpu_air_prover_config.json")
-        .arg("--parameter_file=resources/CairoZero/cpu_air_params.json")
+        .arg("--public_input_file")
+        .arg(&public_input_file)
+        .arg("--private_input_file")
+        .arg(&private_input_file)
+        .arg("--prover_config_file")
+        .arg(&config_file)
+        .arg("--parameter_file")
+        .arg(&params_file)
         .arg("-generate_annotations")
         .arg("--out_file")
-        .arg(proof_path.clone());
+        .arg(&proof_path);
 
     let mut child_proof = command_proof.spawn()?;
     let _status_proof = child_proof.wait()?;
 
-    let result = fs::read_to_string(proof_path).await?;
+    let result = fs::read_to_string(&proof_path).await?;
     let proof: Value = serde_json::from_str(&result)?;
     let final_result = serde_json::to_string_pretty(&proof)?;
+
+    fs::remove_file(&program_input_path).await?;
+    fs::remove_file(&program_path).await?;
+    fs::remove_file(&proof_path).await?;
+    fs::remove_file(&trace_file).await?;
+    fs::remove_file(&memory_file).await?;
+    fs::remove_file(&public_input_file).await?;
+    fs::remove_file(&private_input_file).await?;
+    fs::remove_file(&params_file).await?;
 
     Ok(final_result)
 }
--- prover/src/prove/cairo1.rs
+++ prover/src/prove/cairo1.rs
@@ -13,9 +13,15 @@ pub async fn root(
     _claims: Claims,
     Json(program_input): Json<Cairo1ProverInput>,
 ) -> Result<String, ProveError> {
-    let program_input_path: PathBuf = Path::new("resources/Cairo/input.txt").to_path_buf();
-    let program_path: PathBuf = Path::new("resources/Cairo/program.json").to_path_buf();
+    let program_input_path: PathBuf = Path::new("resources/cairo/input.txt").to_path_buf();
+    let program_path: PathBuf = Path::new("resources/cairo/program.json").to_path_buf();
     let proof_path: PathBuf = Path::new("program_proof_cairo1.json").to_path_buf();
+    let trace_file = Path::new("resources/cairo/program_trace.trace").to_path_buf();
+    let memory_file = Path::new("resources/cairo/program_memory.memory").to_path_buf();
+    let public_input_file = Path::new("resources/cairo/program_public_input.json").to_path_buf();
+    let private_input_file = Path::new("resources/cairo/program_private_input.json").to_path_buf();
+    let params_file = Path::new("resources/cairo/cpu_air_params.json").to_path_buf();
+    let config_file = Path::new("config/cpu_air_prover_config.json").to_path_buf();
 
     let input = serde_json::to_string(&program_input.program_input)?;
     let program = serde_json::to_string(&program_input.program)?;
@@ -36,47 +42,64 @@ pub async fn root(
         std::process::exit(1);
     }
 
-    fs::write(&program_path, program.clone()).await?;
+    fs::write(&program_path, &program).await?;
 
     //run cairo1-run
     let mut command = Command::new("cairo1-run");
     command
-        .arg("--trace_file=resources/Cairo/program_trace.trace")
-        .arg("--memory_file=resources/Cairo/program_memory.memory")
+        .arg("--trace_file")
+        .arg(&trace_file)
+        .arg("--memory_file")
+        .arg(&memory_file)
         .arg("--layout")
         .arg(layout)
         .arg("--proof_mode")
-        .arg("--air_public_input=resources/Cairo/program_public_input.json")
-        .arg("--air_private_input=resources/Cairo/program_private_input.json")
+        .arg("--air_public_input")
+        .arg(&public_input_file)
+        .arg("--air_private_input")
+        .arg(&private_input_file)
         .arg("--args_file")
-        .arg(program_input_path)
-        .arg(program_path.clone());
+        .arg(&program_input_path)
+        .arg(&program_path);
 
     let mut child = command.spawn()?;
     let _status = child.wait()?;
 
     generate_config::generate(
-        "resources/Cairo/program_public_input.json",
-        "resources/Cairo/cpu_air_params.json",
+        "resources/cairo/program_public_input.json",
+        "resources/cairo/cpu_air_params.json",
     );
 
     //run cpu_air_prover
     let mut command_proof = Command::new("cpu_air_prover");
     command_proof
         .arg("--out_file")
-        .arg(proof_path.clone())
-        .arg("--private_input_file=resources/Cairo/program_private_input.json")
-        .arg("--public_input_file=resources/Cairo/program_public_input.json")
-        .arg("--prover_config_file=examples/Cairo/cpu_air_prover_config.json")
-        .arg("--parameter_file=resources/Cairo/cpu_air_params.json")
+        .arg(&proof_path)
+        .arg("--private_input_file")
+        .arg(&private_input_file)
+        .arg("--public_input_file")
+        .arg(&public_input_file)
+        .arg("--prover_config_file")
+        .arg(&config_file)
+        .arg("--parameter_file")
+        .arg(&params_file)
         .arg("-generate-annotations");
 
     let mut child_proof = command_proof.spawn()?;
     let _status_proof = child_proof.wait()?;
 
-    let result = fs::read_to_string(proof_path).await?;
+    let result = fs::read_to_string(&proof_path).await?;
     let proof: Value = serde_json::from_str(&result)?;
     let final_result = serde_json::to_string_pretty(&proof)?;
+
+    fs::remove_file(&program_input_path).await?;
+    fs::remove_file(&program_path).await?;
+    fs::remove_file(&proof_path).await?;
+    fs::remove_file(&trace_file).await?;
+    fs::remove_file(&memory_file).await?;
+    fs::remove_file(&public_input_file).await?;
+    fs::remove_file(&private_input_file).await?;
+    fs::remove_file(&params_file).await?;
 
     Ok(final_result)
 }
