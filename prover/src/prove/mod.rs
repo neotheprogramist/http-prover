5ca16a2fb43fe91280aa5e7167aba468e20b1543
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2024-08-14 11:06:17 +0200

    workflow test, fix root test

--- prover/src/prove/mod.rs
+++ prover/src/prove/mod.rs
@@ -17,8 +17,8 @@ mod tests {
     use super::*;
     use crate::auth::jwt::Claims;
     use axum::Json;
-    use cairo0::root;
-    use common::Cairo0ProverInput;
+    use cairo1::root;
+    use common::Cairo1ProverInput;
     use errors::ProveError;
     use tokio::fs::File;
     use tokio::io::AsyncReadExt;
@@ -26,10 +26,10 @@ mod tests {
     #[tokio::test]
     async fn test_root_with_input_json() {
         // Read input data from resources/input.json
-        let input_json = read_json_file("../examples/CairoZero/prover_input.json")
+        let input_json: Cairo1ProverInput = read_json_file("../examples/Cairo/fibonacci_prover_input.json")
             .await
             .expect("Failed to read input JSON");
-
+        
         // Call the root function with the input data and actual PodmanRunner
         let result = root(
             Claims {
@@ -45,12 +45,12 @@ mod tests {
         // Add assertions based on the expected behavior of root function
     }
 
-    async fn read_json_file(file_path: &str) -> Result<Cairo0ProverInput, ProveError> {
+    async fn read_json_file(file_path: &str) -> Result<Cairo1ProverInput, ProveError> {
         let mut file = File::open(file_path).await?;
         let mut json_string = String::new();
         file.read_to_string(&mut json_string).await?;
 
-        let json_value: Cairo0ProverInput = serde_json::from_str(&json_string)?;
+        let json_value: Cairo1ProverInput = serde_json::from_str(&json_string)?;
 
         Ok(json_value)
     }
